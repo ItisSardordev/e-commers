3adf55602e1df215f29c932766c959356f24a71d
Sardor Akbarovich <sardor.cm.1287@gmail.com>
2025-10-04 22:47:03 +0500

    teeeeeesddd

--- public/admin.js
+++ public/admin.js
@@ -22,11 +22,14 @@ document.getElementById("admin-login").onclick = async () => {
   if (!email || !password)
     return (loginMsg.innerText = "Email va parol kiriting");
   try {
-    const res = await fetch(API + "/admin/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    const res = await fetch(
+      API + "https://e-commers-ashy.vercel.app//admin/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
     const data = await res.json();
     if (data.token) {
       adminToken = data.token;
